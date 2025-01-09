21c085f0a6dcecd7a106408999778af18ce2ca86
kirrtann <sonagrakirtan@gmail.com>
2025-01-09 12:42:55 +0530

    live

--- src/app/common/page.tsx
+++ src/app/common/page.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import Link from 'next/link';
-import React from 'react'
 
 interface CommonCardProps {
   imageurl: string;
@@ -8,35 +8,34 @@ interface CommonCardProps {
   Name: string;
   price: number;
 }
-const CommonCard = ({ imageurl, width, height, Name, price }: CommonCardProps) => {
 
+const CommonCard: React.FC<CommonCardProps> = ({ imageurl, width, height, Name, price }) => {
   const backgroundStyle = {
     backgroundImage: `url(${imageurl})`,
     backgroundSize: 'cover',
     backgroundPosition: 'bottom',
     width: width,
-    height: height
-  }
-
+    height: height,
+  };
 
   return (
-    <>
-      <div>
-        <div className="relative  "
-          style={backgroundStyle}
+    <div>
+      <div className="relative" style={backgroundStyle}>
+        <Link
+          href="/"
+          className="absolute w-full h-full opacity-0 z-10 transition-opacity duration-300 hover:opacity-100"
         >
-          <Link href="/" className=" absolute w-full h-full opacity-0 z-10 transition-opacity duration-300 hover:opacity-100 ">
-            <button className='uppercase bg-white  absolute bottom-5 tracking-[2px] w-[80%] text-[12px] font-bold py-4 px-2 ml-10 mt-10 text-black '>
-              expolre mug
-            </button>
-          </Link>
-        </div>
-        <div className='mt-8 text-center'>
-          <h3 className='text-[#1d1f2e] text-[20px] mb-[5px] mt-[10px]  '>  {Name} </h3>
-          <p className='text-[16px] text-[#1d1f2e] mb-[15px]'>$ {price} USD</p></div>
+          <button className="uppercase bg-white absolute bottom-5 tracking-[2px] w-[80%] text-[12px] font-bold py-4 px-2 ml-10 mt-10 text-black">
+            explore mug
+          </button>
+        </Link>
       </div>
-    </>
-  )
-}
+      <div className="mt-8 text-center">
+        <h3 className="text-[#1d1f2e] text-[20px] mb-[5px] mt-[10px]">{Name}</h3>
+        <p className="text-[16px] text-[#1d1f2e] mb-[15px]">${price} USD</p>
+      </div>
+    </div>
+  );
+};
 
-export default CommonCard
+export default CommonCard;
